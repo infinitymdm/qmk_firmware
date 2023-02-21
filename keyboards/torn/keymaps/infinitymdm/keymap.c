c5300f77f58a3adb150c728c21fc4c44d0129f36
Marcus <infinitymdm@gmail.com>
2023-02-21 12:35:16 -0600

    Add encoder keycodes. May still need a bit of work

--- keyboards/torn/keymaps/infinitymdm/keymap.c
+++ keyboards/torn/keymaps/infinitymdm/keymap.c
@@ -24,6 +24,13 @@ enum torn_layers { _QWERTY, _LOWER, _RAISE, _ADJUST };
 #define G_ENT LGUI_T(KC_ENT)
 #define L_SPC LT(_LOWER, KC_SPC)
 
+// TODO: Encoder keycodes
+/* enum custom_keycodes {
+    LK_CTRL = SAFE_RANGE, // Locking CTRL key
+    LK_ALT, // Locking ALT key
+}; */
+#define KC_TTY C(A(KC_T))
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -35,15 +42,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
  * | \    |   Z  |   X  |   C  |   V  |   B  |    |   N  |   M  |   ,  |   .  |   /  |  ]   |
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
- *               | ____ | Ctrl | Bksp | Del  |    |Enter |Space | Alt  | ____ |
- *               |      |      |Shift |Raise |    | Gui  |Lower |      |      |
+ *               | tty  | Ctrl | Bksp | Del  |    |Enter |Space | Alt  | Caps |
+ *               |      |      |Shift |Raise |    | Gui  |Lower |      | Lock |
  *               `---------------------------'    `---------------------------'
  */
 [_QWERTY] = LAYOUT_split_3x6_4(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
     KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_LBRC,
     KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RBRC ,
-                      R_DEL,   KC_LCTL, S_BSPC,  R_DEL,     G_ENT,   L_SPC,   KC_RALT, G_ENT
+                      KC_TTY,  KC_LCTL, S_BSPC,  R_DEL,     G_ENT,   L_SPC,   KC_RALT, KC_CAPS
 ),
 
 /* Lower
@@ -62,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
     KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PMNS,
     KC_UNDS, _______, _______, _______, _______, _______,   _______, _______, KC_COMM, KC_DOT,  KC_SLSH, KC_PPLS,
-                      _______, _______, _______, _______,   _______, _______, _______, _______
+                      _______,  _______, _______, _______,   _______, _______, _______, KC_MUTE
 ),
 
 /* Raise
@@ -78,10 +85,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `---------------------------'    `---------------------------'
  */
 [_RAISE] = LAYOUT_split_3x6_4(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     _______, _______, _______, _______, _______, _______,
     _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,    _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-    _______, KC_F11,  KC_F12,  _______, _______, _______,   _______, _______, _______, _______, _______, _______,
-                      _______, _______, _______, _______,   _______, _______, _______, _______
+    _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,    _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
+                      _______, _______, _______, _______,   _______, _______, _______, KC_MPLY
 ),
 
 /* Adjust (Lower + Raise)
@@ -104,3 +111,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 
 };
+
+const uint16_t PROGMEM encoder_keymaps[][2][2] = {
+    /* LAYER         LEFT CW       LEFT CCW           RIGHT CW       RIGHT CCW */
+    [_QWERTY] =  { { C(KC_TAB),    C(S(KC_TAB)) },  { C(KC_Y),       C(KC_Z) } },
+    [_LOWER]  =  { { A(KC_TAB),    A(S(KC_TAB)) },  { KC_VOLU,       KC_VOLD } },
+    [_RAISE]  =  { { C(A(KC_RGHT)),C(A(KC_LEFT))},  { KC_MNXT,       KC_MPRV } },
+    [_ADJUST] =  { { _______,      _______ },       { _______,       _______ } },
+};
+// clang-format on
